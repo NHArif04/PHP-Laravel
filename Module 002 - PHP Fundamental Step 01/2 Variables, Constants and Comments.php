abcffbb9cfe2a9b4444fb223f76102d226ed79ea
NHArif04 <nahidhasanarif03@gmail.com>
2023-02-06 22:14:18 +0600

    assignment no 2

--- Module 002 - PHP Fundamental Step 01/2 Variables, Constants and Comments.php
+++ Module 002 - PHP Fundamental Step 01/2 Variables, Constants and Comments.php
@@ -1,14 +1,23 @@
 <?php
 //By this double slash this line is hidden from compiler but it should be inside of start and end syntax;
-$var = "first variable"; //any data type can be define using $ sign
-/**Variable's data types:
- * Integer int 
- * Double / Float
- * String
- * 
- */
+$var = "first variable\n"; //any data type can be define using $ sign
+/** 
+Variable's data types:
+Integer int 
+Double / Float
+String
+Void
+Boolean bool 
+Resource
+Object
+*/
 echo $var;
-$var = 12;       //changing variable's value with another data type
-echo "\n{$var}"; // "\n" is for print new line
-                 //End syntax is not necessary
+var_dump(strrev($var)); 
+//strinng reverse and print size, type and value using var_dump 
+$var = 12;       
+//changing variable's value with another data type
+echo "{$var} \n"; 
+// "\n" is for print new line
+var_dump(strrev($var));
+//End syntax is not necessary
 ?>
---
+++ PracticeCode/max using switch case.php
@@ -0,0 +1,16 @@
+<?php
+$a=10;
+$b=20;
+$c=15;
+
+switch(true){
+    case ($a>$b):
+            $a=$b;
+        break;
+    case ($a>$b):
+        break;
+    case :
+        break;
+    default:
+        break;
+}
