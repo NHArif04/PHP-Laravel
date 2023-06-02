b46fe62b0d0216e6118d6632515ff0b81dfa86e3
Nahid Hasan Arif <nahidhasanarif03@gmail.com>
2023-06-02 18:05:53 +0600

    laravel update

--- PracticeCode/max using switch case.php
+++ PracticeCode/max using switch case.php
@@ -3,14 +3,20 @@ $a=10;
 $b=20;
 $c=15;
 
+$a = (int)readline();
+
 switch(true){
-    case ($a>$b):
-            $a=$b;
+    case ($a>$b && $a>$c):
+        echo $a;
+        break;    
+        
+    case ($a>$b && $a<$c):
+        echo $c;
         break;
-    case ($a>$b):
-        break;
-    case :
-        break;
+
     default:
+        echo $b;
         break;
 }
+
+echo " is the largest of three";
